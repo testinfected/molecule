3933d43410963a6d92f6b2780fd86185d180fa19
testinfected <vtence@gmail.com>
2014-02-22 08:51:01 -0500

    A simple 'Hello, World' example

---
+++ examples/hello-world/src/com/vtence/molecule/examples/helloworld/HelloWorld.java
@@ -0,0 +1,21 @@
+package com.vtence.molecule.examples.helloworld;
+
+import com.vtence.molecule.Application;
+import com.vtence.molecule.Request;
+import com.vtence.molecule.Response;
+import com.vtence.molecule.simple.SimpleServer;
+
+import java.io.IOException;
+
+public class HelloWorld {
+
+    public static void main(String[] args) throws IOException {
+        SimpleServer server = new SimpleServer(8080);
+
+        server.run(new Application() {
+            public void handle(Request request, Response response) throws Exception {
+                response.body("Hello, World");
+            }
+        });
+    }
+}
