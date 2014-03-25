1d7720f75e1eda35a51469d0b942413c0c497656
testinfected <vtence@gmail.com>
2014-03-25 19:19:41 -0400

    Route to index file when static asset request targets a directory

--- src/main/java/com/vtence/molecule/middlewares/StaticAssets.java
+++ src/main/java/com/vtence/molecule/middlewares/StaticAssets.java
@@ -3,27 +3,37 @@ package com.vtence.molecule.middlewares;
 import com.vtence.molecule.Application;
 import com.vtence.molecule.Request;
 import com.vtence.molecule.Response;
+import com.vtence.molecule.util.RequestWrapper;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import static com.vtence.molecule.middlewares.StaticAssets.RequestOverride.override;
+
 public class StaticAssets extends AbstractMiddleware {
 
     private final Application fileServer;
     private final List<String> urls = new ArrayList<String>();
+    private String indexFile;
 
     public StaticAssets(Application fileServer, String... urls) {
         this.fileServer = fileServer;
         this.urls.addAll(Arrays.asList(urls));
     }
 
-    public void serve(String... urls) {
+    public StaticAssets serve(String... urls) {
         this.urls.addAll(Arrays.asList(urls));
+        return this;
+    }
+
+    public StaticAssets index(String indexFile) {
+        this.indexFile = indexFile;
+        return this;
     }
 
     public void handle(Request request, Response response) throws Exception {
-        if (assetRequested(request.pathInfo())) {
+        if (canServe(request.pathInfo())) {
             serve(request, response);
         } else {
             forward(request, response);
@@ -31,13 +41,45 @@ public class StaticAssets extends AbstractMiddleware {
     }
 
     private void serve(Request request, Response response) throws Exception {
+        if (targetsDirectory(request)) {
+            request = override(request).withPath(request.pathInfo() + indexFile);
+        }
         fileServer.handle(request, response);
     }
 
-    private boolean assetRequested(String path) throws Exception {
+    private boolean targetsDirectory(Request request) {
+        return request.pathInfo().endsWith("/");
+    }
+
+    private boolean canServe(String path) throws Exception {
+        return routeDefinedFor(path);
+    }
+
+    private boolean routeDefinedFor(String path) {
         for (String url : urls) {
             if (path.startsWith(url)) return true;
         }
         return false;
+    }
+
+    public static class RequestOverride extends RequestWrapper {
+        private String path;
+
+        public static RequestOverride override(Request request) {
+            return new RequestOverride(request);
+        }
+
+        public RequestOverride(Request request) {
+            super(request);
+        }
+
+        public RequestOverride withPath(String path) {
+            this.path = path;
+            return this;
+        }
+
+        public String pathInfo() {
+            return path != null ? path : super.pathInfo();
+        }
     }
 }
--- src/test/java/com/vtence/molecule/middlewares/StaticAssetsTest.java
+++ src/test/java/com/vtence/molecule/middlewares/StaticAssetsTest.java
@@ -1,51 +1,63 @@
 package com.vtence.molecule.middlewares;
 
 import com.vtence.molecule.Application;
-import com.vtence.molecule.HttpStatus;
 import com.vtence.molecule.Request;
 import com.vtence.molecule.Response;
 import com.vtence.molecule.support.MockRequest;
 import com.vtence.molecule.support.MockResponse;
-import org.jmock.Expectations;
 import org.jmock.integration.junit4.JUnitRuleMockery;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
-import static com.vtence.molecule.HttpStatus.OK;
 import static com.vtence.molecule.support.MockRequest.aRequest;
 import static com.vtence.molecule.support.MockResponse.aResponse;
 
 public class StaticAssetsTest {
 
     @Rule public JUnitRuleMockery context = new JUnitRuleMockery();
-    Application fileServer = context.mock(Application.class, "file server");
+    Application fileServer = new Application() {
+        public void handle(Request request, Response response) throws Exception {
+            response.body(request.pathInfo());
+        }
+    };
     StaticAssets assets = new StaticAssets(fileServer, "/favicon.ico");
 
     MockRequest request = aRequest();
     MockResponse response = aResponse();
 
+    @Before public void
+    setUpResponseChain() {
+        assets.connectTo(new Application() {
+            public void handle(Request request, Response response) throws Exception {
+                response.body("Forwarded");
+            }
+        });
+    }
+
     @Test public void
-    routesToFileServerWhenPathIsMatched() throws Exception {
-        assets.serve("/static");
-        context.checking(new Expectations() {{
-            exactly(2).of(fileServer).handle(with(request), with(response));
-        }});
+    servesFileWhenPathMatchesExactly() throws Exception {
         assets.handle(request.withPath("/favicon.ico"), response);
-        assets.handle(request.withPath("/static"), response);
+        response.assertBody("/favicon.ico");
+    }
+
+    @Test public void
+    servesFileWhenPathMatchesUrlPrefix() throws Exception {
+        assets.serve("/assets");
+        assets.handle(request.withPath("/assets/images/logo.png"), response);
+        response.assertBody("/assets/images/logo.png");
+    }
+
+    @Test public void
+    servesIndexFileIfPathIndicatesADirectory() throws Exception {
+        assets.serve("/faq").index("index.html");
+        assets.handle(request.withPath("/faq/"), response);
+        response.assertBody("/faq/index.html");
     }
 
     @Test public void
     forwardsWhenPathIsNotMatched() throws Exception {
-        assets.connectTo(respondWith(OK));
-        assets.handle(request.withPath("/home"), response);
-        response.assertStatus(OK);
-    }
-
-    private Application respondWith(final HttpStatus status) {
-        return new Application() {
-            public void handle(Request request, Response response) throws Exception {
-                response.status(status);
-            }
-        };
+        assets.handle(request.withPath("/"), response);
+        response.assertBody("Forwarded");
     }
 }
