049fc1e7c6b2e4bf84999ff47710352dc582839a
testinfected <vtence@gmail.com>
2014-04-10 23:08:55 -0400

    This is getting annoying

--- .scripts/publish-to-sonatype.sh
+++ .scripts/publish-to-sonatype.sh
@@ -3,17 +3,20 @@
 if [ "$TRAVIS_REPO_SLUG" == "testinfected/molecule" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
   if [[ $(gradle -q version) != *SNAPSHOT* ]]; then
     echo 'Travis will only publish snapshots. instance.'
+    exit 0
+  fi
+
+  echo -e "Publishing to Sonatype OSS Maven Repository...\n"
+
+  gradle uploadArchives -PnexusUsername="${SONATYPE_USERNAME}" -PnexusPassword="${SONATYPE_PASSWORD}"
+
+  RETVAL=$?
+
+  if [ $RETVAL -eq 0 ]; then
+      echo -e '\nPublished!'
+      exit 0
   else
-    echo "Publishing to Sonatype OSS Maven Repository...\n"
-
-    gradle uploadArchives -PnexusUsername="${SONATYPE_USERNAME}" -PnexusPassword="${SONATYPE_PASSWORD}"
-
-    RETVAL=$?
-
-    if [ $RETVAL -eq 0 ]; then
-        echo '\nPublished!'
-    else
-        echo '\nPublish failed.'
-    fi
+      echo -e '\nPublish failed.'
+      exit 1
   fi
 fi
