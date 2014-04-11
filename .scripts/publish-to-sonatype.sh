aa51661e870c6a54281b40f4d59ec4034048001c
testinfected <vtence@gmail.com>
2014-04-10 21:53:48 -0400

    Publish snapshot to Sonatype OSS maven repo if build succeeds

---
+++ .scripts/publish-to-sonatype.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ "$TRAVIS_REPO_SLUG" == "testinfected/molecule" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+  if [[ $(gradle -q version) != *SNAPSHOT* ]]; then
+      echo 'Travis will only publish snapshots.
+      instance.'
+      return 0
+  fi
+
+  echo -e "Publishing to Sonatype OSS Maven Repository...\n"
+
+  gradle uploadArchives -PnexusUsername="${SONATYPE_USERNAME}"
+  -PnexusPassword="${SONATYPE_PASSWORD}"
+  RETVAL=$?
+
+  if [ $RETVAL -eq 0 ]; then
+    echo 'Published!'
+  else
+    echo 'Publish failed.'
+    return 1
+  fi
+
+fi
