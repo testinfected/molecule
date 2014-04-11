7c1a618ba9efce2a9cbd7a4c2bc26b25f4d3cde5
testinfected <vtence@gmail.com>
2014-04-10 23:01:10 -0400

    Return codes are not allowed in after_success hook

--- .scripts/publish-to-sonatype.sh
+++ .scripts/publish-to-sonatype.sh
@@ -2,23 +2,18 @@
 
 if [ "$TRAVIS_REPO_SLUG" == "testinfected/molecule" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
   if [[ $(gradle -q version) != *SNAPSHOT* ]]; then
-      echo 'Travis will only publish snapshots.
-      instance.'
-      return 0
-  fi
-
-  echo "Publishing to Sonatype OSS Maven Repository...\n"
-
-  gradle uploadArchives -PnexusUsername="${SONATYPE_USERNAME}" -PnexusPassword="${SONATYPE_PASSWORD}"
-
-  RETVAL=$?
-
-  if [ $RETVAL -eq 0 ]; then
-    echo 'Published!'
-    return 0
+    echo 'Travis will only publish snapshots. instance.'
   else
-    echo 'Publish failed.'
-    return 1
-  fi
+    echo "Publishing to Sonatype OSS Maven Repository...\n"
 
+    gradle uploadArchives -PnexusUsername="${SONATYPE_USERNAME}" -PnexusPassword="${SONATYPE_PASSWORD}"
+
+    RETVAL=$?
+
+    if [ $RETVAL -eq 0 ]; then
+        echo '\nPublished!'
+    else
+        echo '\nPublish failed.'
+    fi
+  fi
 fi
