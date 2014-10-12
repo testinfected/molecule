31aafa8cc2051570dd4eb3dc4019b13749fdd87b
testinfected <vtence@gmail.com>
2014-10-11 21:25:03 -0400

    Improve templating and layout example

--- src/test/java/examples/templating/TemplatingAndLayoutExample.java
+++ src/test/java/examples/templating/TemplatingAndLayoutExample.java
@@ -6,22 +6,22 @@ import com.vtence.molecule.Response;
 import com.vtence.molecule.WebServer;
 import com.vtence.molecule.middlewares.Layout;
 import com.vtence.molecule.routing.DynamicRoutes;
-import com.vtence.molecule.support.ResourceLocator;
 import com.vtence.molecule.templating.JMustacheRenderer;
 import com.vtence.molecule.templating.Template;
 import com.vtence.molecule.templating.Templates;
 
-import java.io.File;
 import java.io.IOException;
+
+import static com.vtence.molecule.support.ResourceLocator.locateOnClasspath;
 
 public class TemplatingAndLayoutExample {
 
     public void run(WebServer server) throws IOException {
-        File templateDir = ResourceLocator.locateOnClasspath("examples/templates");
         // We use Mustache templates with an .html extension
-        Templates templates = new Templates(new JMustacheRenderer().fromDir(templateDir).extension("html"));
+        Templates templates = new Templates(
+                new JMustacheRenderer().fromDir(locateOnClasspath("examples/templates")).extension("html"));
         final Template layout = templates.named("layout");
-        final Template hello = templates.named("greeting");
+        final Template greeting = templates.named("greeting");
 
         // Apply a common layout to all rendered pages
         server.filter("/", Layout.html(layout))
@@ -29,13 +29,20 @@ public class TemplatingAndLayoutExample {
                   get("/hello").to(new Application() {
                       public void handle(final Request request, final Response response) throws Exception {
                           response.contentType("text/html; charset=utf-8");
+                          String name = request.parameter("name") != null ? request.parameter("name") : "World";
                           // Mustache can use any object or a Map as a rendering context
-                          response.body(hello.render(new Object() {
-                              String name = request.parameter("name") != null ? request.parameter("name") : "World";
-                          }));
+                          response.body(greeting.render(new User(name)));
                       }
                   });
               }});
+    }
+
+    private static class User {
+        public final String name;
+
+        public User(String name) {
+            this.name = name;
+        }
     }
 
     public static void main(String[] args) throws IOException {
