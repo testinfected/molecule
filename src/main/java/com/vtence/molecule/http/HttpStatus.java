e2579bd6585d616f74f7ce2b496a10276a44d192
testinfected <vtence@gmail.com>
2015-02-02 23:16:26 -0500

    Clarify and update documentation of HTTP status codes

--- src/main/java/com/vtence/molecule/http/HttpStatus.java
+++ src/main/java/com/vtence/molecule/http/HttpStatus.java
@@ -16,17 +16,17 @@ public enum HttpStatus {
     SWITCHING_PROTOCOLS(101, "Switching Protocols"),
 
     /**
-     * This represents a successful response of a targeted request.
+     * Indicates that the request has succeeded.
      */
     OK(200, "OK"),
 
     /**
-     * This is used to signify that a resource was created successfully.
+     * Indicates that the request has resulted in one or more new resources being successfully created.
      */
     CREATED(201, "Created"),
 
     /**
-     * This is used to signify that the request has been accepted.
+     * Indicates that the request has been accepted for processing, but the processing has not been completed.
      */
     ACCEPTED(202, "Accepted"),
 
@@ -36,177 +36,184 @@ public enum HttpStatus {
     NON_AUTHORITATIVE_INFORMATION(203, "Non-Authoritative Information"),
 
     /**
-     * This represents a response that contains no response content.
+     * Indicates that the server has successfully fulfilled the request and that
+     * there is no additional content to send.
      */
     NO_CONTENT(204, "No Content"),
 
     /**
-     * This is used to represent a response that resets the content.
+     * Indicates that indicates that the server successfully fulfilled the request and
+     * desires that the user agent reset the view to its original state.
      */
     RESET_CONTENT(205, "Reset Content"),
 
     /**
-     * This is used to represent a response that has partial content.
+     * Indicates that the server is successfully fulfilling a range request for the target resource.
      */
     PARTIAL_CONTENT(206, "Partial Content"),
 
     /**
-     * This is used to represent a response where there are choices.
+     * Indicates that the target resource has more than one representation.
      */
     MULTIPLE_CHOICES(300, "Multiple Choices"),
 
     /**
-     * This is used to represent a target resource that has moved.
+     * Indicates that the target resource has been assigned a new permanent URI.
      */
     MOVED_PERMANENTLY(301, "Moved Permanently"),
 
     /**
-     * This is used to represent a resource that has been found.
+     * Indicates that the target resource resides temporarily under a different URI.
      */
     FOUND(302, "Found"),
 
     /**
-     * This is used to tell the client to see another HTTP resource.
+     * Indicates that the server is redirecting the user agent to a different resource.
      */
     SEE_OTHER(303, "See Other"),
 
     /**
-     * This is used in response to a target that has not been modified.
+     * Indicates that the client already has a valid representation of the target resource.
      */
     NOT_MODIFIED(304, "Not Modified"),
 
     /**
-     * This is used to tell the client that it should use a proxy.
+     * This code is now deprecated.
      */
     USE_PROXY(305, "Use Proxy"),
 
     /**
-     * This is used to redirect the client to a resource that has moved.
+     * Indicates that the target resource resides temporarily under a different URI. This is different from 302
+     * in that it does not allow changing the request method (i.e. from POST to GET) when redirecting.
      */
     TEMPORARY_REDIRECT(307, "Temporary Redirect"),
 
     /**
-     * This is used to tell the client they have send an invalid request.
+     * Indicates that the server will not process the request due a client error (e.g. a malformed request syntax)
      */
     BAD_REQUEST(400, "Bad Request"),
 
     /**
-     * This is used to tell the client that authorization is required.
+     * Indicates that the request lacks valid authentication credentials for the target resource.
      */
     UNAUTHORIZED(401, "Unauthorized"),
 
     /**
-     * This is used to tell the client that payment is required.
+     * This is reserved for future use.
      */
     PAYMENT_REQUIRED(402, "Payment Required"),
 
     /**
-     * This is used to tell the client that the resource is forbidden.
+     * Indicates that the server refuses to authorize the request.
      */
     FORBIDDEN(403, "Forbidden"),
 
     /**
-     * This is used to tell the client that the resource is not found.
+     * Indicates that the server did not find a representation for the target resource.
      */
     NOT_FOUND(404, "Not Found"),
 
     /**
-     * This is used to tell the client that the method is not allowed.
+     * Indicates that that the method is not supported by the target resource.
      */
     METHOD_NOT_ALLOWED(405, "Method Not Allowed"),
 
     /**
-     * This is used to tell the client the request is not acceptable.
+     * Indicates that the target resource does not have an acceptable representation
+     * according to content negotiation headers.
      */
     NOT_ACCEPTABLE(406, "Not Acceptable"),
 
     /**
-     * This is used to tell the client that authentication is required.
+     * Indicates that the client needs to authenticate itself in order to use a proxy.
      */
     PROXY_AUTHENTICATION_REQUIRED(407, "Proxy Authentication Required"),
 
     /**
-     * This is used to tell the client that the request has timed out.
+     * Indicates that the server did not receive a complete request message within the time that it was
+     * prepared to wait.
      */
     REQUEST_TIMEOUT(408, "Request Timeout"),
 
     /**
-     * This is used to tell the client that there has been a conflict.
+     * Indicates that the request could not be completed due to a conflict
+     * with the current state of the target resource.
      */
     CONFLICT(409, "Conflict"),
 
     /**
-     * This is used to tell the client that the resource has gone.
+     * Indicates that access to the target resource is no longer available.
      */
     GONE(410, "Gone"),
 
     /**
-     * This is used to tell the client that a request length is needed.
+     * Indicates that the server refuses to accept the request without a defined Content-Length.
      */
     LENGTH_REQUIRED(411, "Length Required"),
 
     /**
-     * This is used to tell the client that a precondition has failed.
+     * Indicates that one or more conditions given in the request header fields failed.
      */
     PRECONDITION_FAILED(412, "Precondition Failed"),
 
     /**
-     * This is used to tell the client that the request body is too big.
+     * Indicates that the server is refusing to process the request because the request payload is too large.
      */
     PAYLOAD_TOO_LARGE(413, "Payload Too Large"),
 
     /**
-     * This is used to tell the client that the request URI is too long.
+     * Indicates that the server is refusing to service the request because the request URI is too long.
      */
     URI_TOO_LONG(414, "URI Too Long"),
 
     /**
-     * This is used to tell the client that the content type is invalid.
+     * Indicates that the server is refuses to service the request because the payload content type is invalid.
      */
     UNSUPPORTED_MEDIA_TYPE(415, "Unsupported Media Type"),
 
     /**
-     * This is used to tell the client that the range is invalid.
+     * Indicates that the request has been rejected due to invalid ranges.
      */
     RANGE_NOT_SATISFIABLE(416, "Range Not Satisfiable"),
 
     /**
-     * Indicates that the server refuses to perform the request using the current protocol.
-     */
-    UPGRADE_REQUIRED(426, "Upgrade Required"),
-
-    /**
-     * This is used to tell the client that the expectation has failed.
+     * Indicates that the client expectations could not be met.
      */
     EXPECTATION_FAILED(417, "Expectation Failed"),
 
     /**
-     * This is sent when the request has caused an internal server error.
+    * Indicates that the server refuses to perform the request using the current protocol.
+    */
+    UPGRADE_REQUIRED(426, "Upgrade Required"),
+
+    /**
+     * Indicates that the server encountered an unexpected error and could not fulfill the request.
      */
     INTERNAL_SERVER_ERROR(500, "Internal Server Error"),
 
     /**
-     * This is used to tell the client the resource is not implemented.
+     * Indicates that the server does not support the functionality required to fulfill the request.
      */
     NOT_IMPLEMENTED(501, "Not Implemented"),
 
     /**
-     * This is used to tell the client that the gateway is invalid.
+     * Indicates that the gateway or proxy received an invalid response
+     * from an inbound server while attempting to fulfill the request.
      */
     BAD_GATEWAY(502, "Bad Gateway"),
 
     /**
-     * This is used to tell the client the resource is unavailable.
+     * Indicates that the server is currently unable to handle the request.
      */
     SERVICE_UNAVAILABLE(503, "Service Unavailable"),
 
     /**
-     * This is used to tell the client there was a gateway timeout.
+     * Indicates that the gateway or proxy did not receive a timely response from an upstream server.
      */
     GATEWAY_TIMEOUT(504, "Gateway Timeout"),
 
     /**
-     * This is used to tell the client the request version is invalid.
+     * Indicates that the server does not support the major version of HTTP that was used in the request message.
      */
     HTTP_VERSION_NOT_SUPPORTED(505, "HTTP Version Not Supported");
 
