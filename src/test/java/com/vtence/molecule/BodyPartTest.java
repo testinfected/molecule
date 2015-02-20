40c7d72ca275680754e38fd8c926ee364d007198
testinfected <vtence@gmail.com>
2015-02-20 18:07:00 -0500

    Make sure our body part test actually proves we're using the right encoding

--- src/test/java/com/vtence/molecule/BodyPartTest.java
+++ src/test/java/com/vtence/molecule/BodyPartTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 
 import java.io.IOException;
 
+import static com.vtence.molecule.helpers.Charsets.UTF_16;
+import static com.vtence.molecule.helpers.Charsets.UTF_8;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
@@ -13,7 +15,7 @@ public class BodyPartTest {
     public void decodesTextContentAccordingToContentTypeCharset() throws IOException {
         String originalText = "Les naïfs ægithales hâtifs pondant à Noël où il gèle...";
 
-        BodyPart part = new BodyPart().contentType("text/plain; charset=utf-16").content(originalText);
+        BodyPart part = new BodyPart().contentType("text/plain; charset=utf-16").content(originalText.getBytes(UTF_16));
 
         assertThat("decoded text", part.content(), equalTo(originalText));
     }
@@ -22,7 +24,7 @@ public class BodyPartTest {
     public void defaultsToUTF8DecodingWhenNoContentTypeIsSpecified() throws IOException {
         String originalText = "sont sûrs d'être déçus...";
 
-        BodyPart part = new BodyPart().content(originalText);
+        BodyPart part = new BodyPart().content(originalText.getBytes(UTF_8));
 
         assertThat("decoded text", part.content(), equalTo(originalText));
     }
@@ -31,7 +33,7 @@ public class BodyPartTest {
     public void defaultsToUTF8DecodingWhenNoCharsetIsSpecified() throws IOException {
         String originalText = "en voyant leurs drôles d'oeufs abîmés.";
 
-        BodyPart part = new BodyPart().contentType("text/plain").content(originalText);
+        BodyPart part = new BodyPart().contentType("text/plain").content(originalText.getBytes(UTF_8));
 
         assertThat("decoded text", part.content(), equalTo(originalText));
     }
