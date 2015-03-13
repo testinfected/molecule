f38e2814241658297d24b1072774dfe260a4498f
testinfected <vtence@gmail.com>
2015-03-13 14:36:16 -0400

    We can now know what cookies the jar holds

--- src/main/java/com/vtence/molecule/lib/CookieJar.java
+++ src/main/java/com/vtence/molecule/lib/CookieJar.java
@@ -3,11 +3,13 @@ package com.vtence.molecule.lib;
 import com.vtence.molecule.Request;
 import com.vtence.molecule.http.Cookie;
 
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 public class CookieJar {
-    private final Map<String, Cookie> cookies = new HashMap<String, Cookie>();
+    private final Map<String, Cookie> cookies = new LinkedHashMap<String, Cookie>();
 
     public static CookieJar get(Request request) {
         return request.attribute(CookieJar.class);
@@ -23,5 +25,21 @@ public class CookieJar {
 
     public void add(Cookie cookie) {
         cookies.put(cookie.name(), cookie);
+    }
+
+    public void add(String name, String value) {
+        add(new Cookie(name, value));
+    }
+
+    public List<Cookie> list() {
+        return new ArrayList<Cookie>(cookies.values());
+    }
+
+    public boolean empty() {
+        return cookies.isEmpty();
+    }
+
+    public boolean has(String name) {
+        return cookies.containsKey(name);
     }
 }
--- src/test/java/com/vtence/molecule/lib/CookieJarTest.java
+++ src/test/java/com/vtence/molecule/lib/CookieJarTest.java
@@ -1,9 +1,15 @@
 package com.vtence.molecule.lib;
 
 import com.vtence.molecule.Request;
+import com.vtence.molecule.http.Cookie;
+import org.hamcrest.FeatureMatcher;
+import org.hamcrest.Matcher;
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.sameInstance;
 
 public class CookieJarTest {
@@ -19,5 +25,42 @@ public class CookieJarTest {
         Request request = new Request();
         jar.bind(request);
         assertThat("bound cookie jar", CookieJar.get(request), sameInstance(jar));
+    }
+
+    @Test
+    public void isInitiallyEmpty() {
+        assertThat("empty jar?", jar.empty(), is(true));
+    }
+
+    @Test
+    public void isNoLongerEmptyOnceItContainsCookies() {
+        jar.add("a cookie", "<value>");
+        assertThat("empty jar?", jar.empty(), is(false));
+    }
+
+    @Test @SuppressWarnings("unchecked")
+    public void maintainsAListOfCookies() {
+        jar.add("mr christie", "peanuts");
+        jar.add("petit ecolier", "chocolat noir");
+        jar.add("delicious", "chocolat au lait");
+
+        assertThat("cookies in jar", jar.list(), contains(
+                cookieNamed("mr christie"), cookieNamed("petit ecolier"), cookieNamed("delicious")));
+    }
+
+    @Test
+    public void knowsWhatCookiesItHolds() {
+        jar.add("some cookie", "<some value>");
+        jar.add("other cookie", "<other value>");
+        assertThat("holds some cookie", jar.has("some cookie"), is(true));
+        assertThat("holds eaten cookie", jar.has("eaten cookie"), is(false));
+    }
+
+    private Matcher<Cookie> cookieNamed(String name) {
+        return new FeatureMatcher<Cookie, String>(equalTo(name), "cookie named", "cookie") {
+            protected String featureValueOf(Cookie cookie) {
+                return cookie.name();
+            }
+        };
     }
 }
