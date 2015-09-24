f608337efe5c42400d4c7e6215051da91940c1ed
testinfected <vtence@gmail.com>
2015-09-23 20:43:50 -0400

    Add inline doc for the helloworld example

--- src/test/java/examples/helloworld/HelloWorldExample.java
+++ src/test/java/examples/helloworld/HelloWorldExample.java
@@ -4,9 +4,15 @@ import com.vtence.molecule.WebServer;
 
 import java.io.IOException;
 
+/**
+ * In this example, we run a single application that responds <i>Hello, World!</i> to all incoming requests.
+ * This is as simple as it can get.
+ */
 public class HelloWorldExample {
 
     public void run(WebServer server) throws IOException {
+        // Start the default web server and provide a single application, which
+        // responds to all incoming requests.
         server.start((request, response) -> response.done("Hello, World!"));
     }
 
