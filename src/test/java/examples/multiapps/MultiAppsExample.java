a1f0c61ad5501c7c723f4a35bfb2aef1c24d0312
testinfected <vtence@gmail.com>
2015-09-28 09:00:39 -0400

    Add javadoc to the multi-apps example

--- src/test/java/examples/multiapps/MultiAppsExample.java
+++ src/test/java/examples/multiapps/MultiAppsExample.java
@@ -6,17 +6,28 @@ import com.vtence.molecule.middlewares.URLMap.MountPoint;
 
 import java.io.IOException;
 
+
+/**
+ * <p>
+ *     In this example we demonstrate attaching several applications to different mount points. Mount points are
+ *     matched as words in definition order. More specific paths have precedence over less specific ones.
+ * </p>
+ * <p>
+ *     This is useful when you want multiple applications to coexist on the server. For instance, you could run both an
+ *     API application under /api and a web application under /.
+ * </p>
+ */
 public class MultiAppsExample {
 
     public void run(WebServer server) throws IOException {
-
-        // This middleware simply prints information about the mount point and request
+        // We will mount a simple application that prints information about the mount point and client request
         Application describe = (request, response) -> {
             // The mount point is available as a request attribute
             MountPoint mount = MountPoint.get(request);
             // The modified request path, stripped of the mounted application prefix
+            // (e.g. this will be /baz for a request to /foo/bar/baz)
             String pathInfo = request.path();
-            // The mounted application path prefix
+            // The mounted application path prefix (i.e. either /foo, /foo/bar or /baz in our example)
             String mountPoint = mount.app();
             // To reconstruct the full uri, which is useful when creating links
             String uri = mount.uri(pathInfo);
@@ -24,8 +35,9 @@ public class MultiAppsExample {
             response.done(String.format("%s at %s (%s)", mountPoint, pathInfo, uri));
         };
 
+        // Mount points are matched in definition order, the most specific match wins
         server.mount("/foo", describe) // matches urls starting with /foo but not /foo/bar
-              .mount("/foo/bar", describe) // matches urls starting with /foo/bar
+              .mount("/foo/bar", describe) // matches urls starting with /foo/bar - wins over /foo since more specific
               .mount("/baz", describe) // matches urls starting with /baz
               // Start the server without a default app, which means all other requests will get a 404
               .start();
