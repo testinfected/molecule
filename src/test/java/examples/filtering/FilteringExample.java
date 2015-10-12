882fb75ba4c58b30864710ee8e33f220614152cc
testinfected <vtence@gmail.com>
2015-10-12 13:33:10 -0400

    Add javadoc to the filtering example

--- src/test/java/examples/filtering/FilteringExample.java
+++ src/test/java/examples/filtering/FilteringExample.java
@@ -13,25 +13,41 @@ import java.util.Map;
 
 import static com.vtence.molecule.http.HttpStatus.UNAUTHORIZED;
 
+/**
+ * <p>
+ *     This example shows how to filter access to specific resources. We use a custom authentication middleware
+ *     to make sure only authenticated users can access resources under <code>/private</code>.
+ * </p>
+ */
 public class FilteringExample {
 
+    /**
+     * Our user database is a simple hash.
+     */
     private final Map<String, String> users = new HashMap<>();
 
-    public FilteringExample() {
+    // Let's populate our users base with a single user "admin" with password "admin"
+    {
         users.put("admin", "admin");
     }
 
+    public FilteringExample() {
+    }
+
     public void run(WebServer server) throws IOException {
-        // An an authentication filter that checks against a map of authorized users
+        // We implement a simple authentication middleware that checks against a map of authorized users
+        // If credentials match, we allow access to the requested resource. If not, we send a 401 - Unauthorized.
         Middleware authenticate = new AbstractMiddleware() {
             public void handle(Request request, Response response) throws Exception {
+                // We read the username and password from the request parameters
                 String user = request.parameter("username");
                 String password = request.parameter("password");
 
                 String token = users.get(user);
                 if (password != null && password.equals(token)) {
+                    // Credentials match, store the current user as a request attribute...
                     request.attribute("user", user);
-                    // Carry on with the processing chain
+                    // ... then carry on with the processing chain
                     forward(request, response);
                 } else {
                     // Halt request processing
@@ -44,11 +60,11 @@ public class FilteringExample {
         // All requests to /private/... go through the authentication filter
         server.filter("/private", authenticate)
               .start(new DynamicRoutes() {{
-                  // This route is private thus requires authentication
+                  // This route is private, thus it requires authentication
                   get("/private/area").to((request, response) ->
                           response.done("Hello, " + request.attribute("user") + "!"));
 
-                  // This route is public
+                  // This route is public, anybody can access it
                   get("/hello").to((request, response) ->
                           response.done("Welcome, Guest!"));
               }});
