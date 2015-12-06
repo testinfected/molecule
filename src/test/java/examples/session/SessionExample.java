705c9d61c5880887cc6fe6257580062fc36ca980
testinfected <vtence@gmail.com>
2015-12-06 14:02:53 -0500

    Add documentation to the session example

--- src/test/java/examples/session/SessionExample.java
+++ src/test/java/examples/session/SessionExample.java
@@ -14,6 +14,21 @@ import java.time.Clock;
 import static java.util.concurrent.TimeUnit.DAYS;
 import static java.util.concurrent.TimeUnit.MINUTES;
 
+/**
+ * Here's an example of setting up HTTP sessions. We use an im-memory session pool but you could very well
+ * swap the pool implementation for, e.g. a signed cookie store session pool.
+ *
+ * <p>
+ *     Each client is given a secure session identifier, stored as session cookie.
+ *     If the client request it, we make the cookie persistent (for 5 min).
+ *     Every time the session is accessed, we refresh the client cookie expiry date.
+ * </p>
+ * <p>
+ *     The session pool expires stale sessions after 30 minutes.
+ *     As a protection mechanism, we also expire sessions that are older than 2 days,
+ *     even if they have been maintained active.
+ * </p>
+ */
 public class SessionExample {
 
     private static final int FIVE_MINUTES = (int) MINUTES.toSeconds(5);
@@ -38,18 +53,28 @@ public class SessionExample {
               // You can change of the name of the cookie used to track sessions
               .add(new CookieSessionTracker(sessionPool).usingCookieName("molecule.session"))
               .start(new DynamicRoutes() {{
+                         // The default route greets the signed in user
                          map("/").to((request, response) -> {
+                             // There's always a session bound to the request, although it may be empty and fresh
+                             // We can safely read a new session. The session won't be saved to the pool unless
+                             // it's been written or it was already present in the pool.
                              Session session = Session.get(request);
+                             // If our user has already identified to our site,
+                             // we have a username stored in the session
                              String username = session.contains("username") ? session.<String>get("username") : "Guest";
                              response.done("Hello, " + username);
                          });
 
+                         // The sign in route
                          post("/login").to((request, response) -> {
+                             // We expect a username parameter
                              String username = request.parameter("username");
                              Session session = Session.get(request);
+                             // Store the username in the session. Since the session has been written to,
+                             // it will automatically be saved to the pool by the end of the request cycle
                              session.put("username", username);
 
-                             // If remember me, make session cookie persistent with a max age of 5 minutes
+                             // If remember me is checked, make session cookie persistent with a max age of 5 minutes
                              boolean rememberMe = request.parameter("remember_me") != null;
                              if (rememberMe) {
                                  session.maxAge(FIVE_MINUTES);
@@ -67,8 +92,11 @@ public class SessionExample {
                              response.redirectTo("/").done();
                          });
 
+                         // The sign out route
                          delete("/logout").to((request, response) -> {
                              Session session = Session.get(request);
+                             // We invalidate the session, which prevents further use and removes the session
+                             // from the pool at the end of the request cycle
                              session.invalidate();
                              response.redirectTo("/").done();
                          });
