3dd350b7ef14f11febe07d253b82e63dcd2cec1f
testinfected <vtence@gmail.com>
2017-09-23 23:47:09 -0400

    Convert `FailureMonitor` middleware to a function
    See #64

--- src/main/java/com/vtence/molecule/middlewares/FailureMonitor.java
+++ src/main/java/com/vtence/molecule/middlewares/FailureMonitor.java
@@ -1,5 +1,6 @@
 package com.vtence.molecule.middlewares;
 
+import com.vtence.molecule.Application;
 import com.vtence.molecule.FailureReporter;
 import com.vtence.molecule.Request;
 import com.vtence.molecule.Response;
@@ -9,6 +10,18 @@ public class FailureMonitor extends AbstractMiddleware {
 
     public FailureMonitor(FailureReporter reporter) {
         this.reporter = reporter;
+    }
+
+
+    public Application then(Application next) {
+        return Application.of(request -> {
+            try {
+                return next.handle(request).whenFailed((result, error) -> reporter.errorOccurred(error));
+            } catch (Throwable error) {
+                reporter.errorOccurred(error);
+                throw error;
+            }
+        });
     }
 
     public void handle(Request request, Response response) throws Exception {
