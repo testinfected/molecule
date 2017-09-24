c841ddd9c2cdcf7b0a4b4cef4d36b8193c4df4c6
testinfected <vtence@gmail.com>
2017-09-24 18:59:54 -0400

    Convert `HttpMethodOverride` middleware to a function
    See #64

--- src/main/java/com/vtence/molecule/middlewares/HttpMethodOverride.java
+++ src/main/java/com/vtence/molecule/middlewares/HttpMethodOverride.java
@@ -1,8 +1,9 @@
 package com.vtence.molecule.middlewares;
 
-import com.vtence.molecule.http.HttpMethod;
+import com.vtence.molecule.Application;
 import com.vtence.molecule.Request;
 import com.vtence.molecule.Response;
+import com.vtence.molecule.http.HttpMethod;
 
 import java.io.IOException;
 
@@ -11,6 +12,15 @@ import static com.vtence.molecule.http.HttpMethod.POST;
 public class HttpMethodOverride extends AbstractMiddleware {
 
     public static final String METHOD_OVERRIDE_PARAMETER = "_method";
+
+    public Application then(Application next) {
+        return Application.of(request -> {
+            if (overrideDetected(request) && validOverride(request)) {
+                request.method(methodOverride(request).toUpperCase());
+            }
+            return next.handle(request);
+        });
+    }
 
     public void handle(Request request, Response response) throws Exception {
         if (overrideDetected(request) && validOverride(request)) {
