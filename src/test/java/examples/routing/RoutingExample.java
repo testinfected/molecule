bc5ebf396240d827585b440d71bb93f4d4eeb64b
testinfected <vtence@gmail.com>
2017-09-25 20:33:07 -0400

    Adapt routing example to application as a function
    See #64

--- src/test/java/examples/routing/RoutingExample.java
+++ src/test/java/examples/routing/RoutingExample.java
@@ -1,5 +1,7 @@
 package examples.routing;
 
+import com.vtence.molecule.Application;
+import com.vtence.molecule.Response;
 import com.vtence.molecule.WebServer;
 import com.vtence.molecule.routing.DynamicRoutes;
 
@@ -7,12 +9,12 @@ import java.io.IOException;
 
 /**
  * <p>
- *     This is a simple example showing how to define routes and how to use route patterns containing named
- *     parameters. Named parameters are accessible as normal request parameters.
+ * This is a simple example showing how to define routes and how to use route patterns containing named
+ * parameters. Named parameters are accessible as normal request parameters.
  * </p>
  * <p>
- *     We want to route incoming requests based on their paths and verbs, so we provide route patterns as strings.
- *     If your matching logic is more sophisticated, you can also provide custom matchers.
+ * We want to route incoming requests based on their paths and verbs, so we provide route patterns as strings.
+ * If your matching logic is more sophisticated, you can also provide custom matchers.
  * </p>
  */
 public class RoutingExample {
@@ -25,29 +27,28 @@ public class RoutingExample {
             // invoked.
 
             // Route all requests to the root path - ignoring the verb - to an endpoint that responds with "Welcome!"
-            map("/").to((request, response) -> response.done("Welcome!"));
+            map("/").to(Application.of(request -> Response.ok().done("Welcome!")));
 
             // Route POST requests to /login to an endpoint that simulates login. It simply reads the username
             // from the request parameters and redirects to /users/username.
-            post("/login").to((request, response) -> {
+            post("/login").to(Application.of(request -> {
                 String username = request.parameter("username");
-                response.redirectTo("/hello/" + username)
-                        .done();
-            });
+                return Response.redirect("/hello/" + username)
+                               .done();
+            }));
 
             // Route GET requests to /hello/username to an endpoint that renders and HTML page with
             // a greeting message for our user. We use a route pattern that includes a named segment (:username).
             // The bound parameter is available as a request parameter.
-            get("/hello/:username").to((request, response) -> {
-                response.contentType("text/html");
-                response.done(
-                        "<html>" +
-                        "<body>" +
-                        "<h3>Hello, " + request.parameter("username") + "</h3>" +
-                        "</body>" +
-                        "</html>"
-                );
-            });
+            get("/hello/:username").to(
+                    Application.of(request -> Response.ok()
+                                                      .contentType("text/html")
+                                                      .done("<html>" +
+                                                            "<body>" +
+                                                            "<h3>Hello, " + request.parameter("username") + "</h3>" +
+                                                            "</body>" +
+                                                            "</html>"
+                                                      )));
         }});
     }
 
