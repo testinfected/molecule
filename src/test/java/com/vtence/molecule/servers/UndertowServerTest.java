69b10beae29742745eee51cc549ef3884e957dee
testinfected <vtence@gmail.com>
2022-03-11 23:43:37 -0500

    Simplify some regex

--- src/test/java/com/vtence/molecule/servers/UndertowServerTest.java
+++ src/test/java/com/vtence/molecule/servers/UndertowServerTest.java
@@ -2,20 +2,12 @@ package com.vtence.molecule.servers;
 
 import com.vtence.molecule.Response;
 import com.vtence.molecule.Server;
-import org.junit.BeforeClass;
 import org.junit.Test;
-
-import java.util.logging.LogManager;
 
 import static com.vtence.molecule.testing.http.HttpResponseAssert.assertThat;
 import static java.net.http.HttpResponse.BodyHandlers.ofString;
 
 public class UndertowServerTest extends ServerCompatibilityTests {
-
-    @BeforeClass public static void
-    silenceLogging() {
-        LogManager.getLogManager().reset();
-    }
 
     protected Server createServer(String host, int port) {
         return new UndertowServer(host, port);
